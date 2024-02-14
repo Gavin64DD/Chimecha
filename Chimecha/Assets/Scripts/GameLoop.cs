368672f44597955f016e397521c7d65441194973
Gavin64DD <shadow10008@outlook.com>
2024-02-14 11:21:55 -0500

    Added displaying for the game loop

--- Chimecha/Assets/Scripts/GameLoop.cs
+++ Chimecha/Assets/Scripts/GameLoop.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GameLoop : MonoBehaviour
 {
+    int numOfPlayers;
+    int currentPlayer;
+    bool waiting;
+    List<List<Card>> cardLists;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        numOfPlayers = 4;
+        waiting = true;
+        currentPlayer = 1;
+
+        //Temporary loop that automatically gives each player 4 sets of random cards
+        for (int i = 0; i < numOfPlayers; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!waiting) return;
+
+
     }
 }
